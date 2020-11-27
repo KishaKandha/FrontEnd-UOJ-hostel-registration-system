abfab27161f47a954a5d6a045d0b3edc23023909
KishaKandha <kishanik1118@gmail.com>
2020-11-27 11:52:00 +0530

    Components1

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React, { useState } from 'react';
 import logo from './logo.svg';
 import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
+import  Menu from  './components/MenuComponent';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    /* navabar component */
+    <div>
     <Navbar dark color="primary">
       <div className="container">
         <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
       </div>
     </Navbar>
+
+   
+    <Menu />
     </div>
   );
 }
