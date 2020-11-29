17ae8d015cc8cc72acaf128ee4fc4e5083042362
KishaKandha <kishanik1118@gmail.com>
2020-11-29 11:22:18 +0530

    Components2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import React, { useState } from 'react';
-import logo from './logo.svg';
-import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
+import React, { Component } from 'react';
+import { Navbar,  NavbarBrand } from 'reactstrap';
 import  Menu from  './components/MenuComponent';
 import './App.css';
+import { DISHES } from './shared/dishes';
 
-function App() {
-  return (
-    /* navabar component */
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES
+    };
+  }
+
+  render(){
+    return (
+   
     <div>
     <Navbar dark color="primary">
       <div className="container">
@@ -15,9 +24,10 @@ function App() {
     </Navbar>
 
    
-    <Menu />
+    <Menu  dishes={this.state.dishes}/>
     </div>
   );
+}
 }
 
 export default App;
