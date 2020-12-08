60d6f5120334b5256b8edc8f175462e342104201
KishaKandha <kishanik1118@gmail.com>
2020-12-08 17:01:22 +0530

    Router

--- src/App.js
+++ src/App.js
@@ -1,36 +1,19 @@
 import React, { Component } from 'react';
 import  Main from  './components/MainComponent';
 import './App.css';
+import { BrowserRouter } from 'react-router-dom';
 
 
 class App extends Component {
-
- /*  constructor(props) {
-    super(props);
-    this.state = {
-      dishes: DISHES
-    };
-
-
-  } ----Everything is included in MainComponent
-  import { Navbar,  NavbarBrand } from 'reactstrap';
-  
-    <Navbar dark color="primary">
-      <div className="container">
-        <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-      </div>
-    </Navbar>
-    <Menu  dishes={this.state.dishes}/>
-  
-  */
-
   render(){
     return (
-   <div> 
-    <Main/>
-   </div>
-  );
-}
+      <BrowserRouter>
+         <div> 
+            <Main/>
+         </div>
+      </BrowserRouter>
+   );
+  }
 }
 
 export default App;
